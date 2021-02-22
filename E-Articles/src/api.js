ec4e72da1a85b74d71ec067ca10abac2637d3f5c
Abdalla <abdallazwolle@hotmail.com>
2021-02-22 14:44:41 +0000

    mixed content errors

--- E-Articles/src/api.js
+++ E-Articles/src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const getArticles = (topic, sort_by, order) => {
   return axios
-    .get(`https://nc-newsapi.herokuapp.com/api/articles`, {
+    .get(`//nc-newsapi.herokuapp.com/api/articles`, {
       params: {
         topic,
         sort_by,
@@ -15,28 +15,28 @@ export const getArticles = (topic, sort_by, order) => {
 };
 export const getTopics = () => {
   return axios
-    .get("https://nc-newsapi.herokuapp.com/api/topics")
+    .get("//nc-newsapi.herokuapp.com/api/topics")
     .then(({ data: { topics } }) => {
       return topics;
     });
 };
 export const getArticle = (id) => {
   return axios
-    .get(`https://nc-newsapi.herokuapp.com/api/articles/${id}`)
+    .get(`//nc-newsapi.herokuapp.com/api/articles/${id}`)
     .then(({ data: { Article } }) => {
       return Article;
     });
 };
 export const getArticleComments = (id) => {
   return axios
-    .get(`https://nc-newsapi.herokuapp.com/api/articles/${id}/comments`)
+    .get(`//nc-newsapi.herokuapp.com/api/articles/${id}/comments`)
     .then(({ data }) => {
       return data;
     });
 };
 // export const sortingArticles = (value) => {
 //   return axios
-//     .get(`https://nc-newsapi.herokuapp.com/api/articles?sort_by=${value}`)
+//     .get(`//nc-newsapi.herokuapp.com/api/articles?sort_by=${value}`)
 //     .then(({ data }) => {
 //       return data;
 //     });
@@ -44,7 +44,7 @@ export const getArticleComments = (id) => {
 export const addCommentById = (input) => {
   return axios
     .post(
-      `https://nc-newsapi.herokuapp.com/api/articles/${input.article_id}/comments`,
+      `//nc-newsapi.herokuapp.com/api/articles/${input.article_id}/comments`,
       input
     )
     .then(({ data }) => {
@@ -53,14 +53,14 @@ export const addCommentById = (input) => {
 };
 export const deleteCommentById = (id) => {
   return axios
-    .delete(`https://nc-newsapi.herokuapp.com/api/comments/${id}`)
+    .delete(`//nc-newsapi.herokuapp.com/api/comments/${id}`)
     .then(({ data }) => {
       return data;
     });
 };
 export const updateStar = (id, starChange) => {
   return axios
-    .patch(`https://nc-newsapi.herokuapp.com/api/articles/${id}`, {
+    .patch(`//nc-newsapi.herokuapp.com/api/articles/${id}`, {
       inc_votes: starChange,
     })
     .then(({ data }) => {
@@ -69,7 +69,7 @@ export const updateStar = (id, starChange) => {
 };
 export const updateCommentVote = (id, starChange) => {
   return axios
-    .patch(`https://nc-newsapi.herokuapp.com/api/comments/${id}`, {
+    .patch(`//nc-newsapi.herokuapp.com/api/comments/${id}`, {
       inc_votes: starChange,
     })
     .then(({ data }) => {
